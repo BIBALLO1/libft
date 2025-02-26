0032573971e78f3942a2ccf5eb8613c02f3db173
biballo <dmoraleda11@gmail.com>
2025-02-26 14:12:31 +0100

    fixed atoi base

--- ft_atoi_base.c
+++ ft_atoi_base.c
@@ -6,7 +6,7 @@
 /*   By: dmoraled <dmoraled@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 17:13:54 by dmoraled          #+#    #+#             */
-/*   Updated: 2025/02/25 11:33:42 by dmoraled         ###   ########.fr       */
+/*   Updated: 2025/02/26 14:07:58 by dmoraled         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,20 @@
 
 int	ft_atoi_base(const char *str, const char *base)
 {
-	int		result;
-	int		i;
-	int		base_len;
-	char	*base_ptr;
+	unsigned int		result;
+	int					base_len;
+	char				*base_ptr;
 
 	base_len = ft_strlen(base);
-	i = 0;
 	result = 0;
-	while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' ')
-		++i;
-	base_ptr = ft_strchr(base, str[i]);
-	while (str[i] && base_ptr)
+	while ((*str >= '\t' && *str <= '\r') || *str == ' ')
+		++str;
+	base_ptr = ft_strchr(base, ft_tolower(*str));
+	while (*str && base_ptr)
 	{
 		result = (result * base_len) + (int)(base_ptr - base);
-		++i;
-		base_ptr = ft_strchr(base, str[i]);
+		++str;
+		base_ptr = ft_strchr(base, ft_tolower(*str));
 	}
 	return (result);
 }
